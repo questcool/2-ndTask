136cb17e1dd2153177d2a2b5a8e5945116745ce1
Kirill <questcool1@gmail.com>
2021-10-04 14:12:27 +0300

    my commit

---
+++ HomeTask2.js
@@ -0,0 +1,69 @@
+function makeObjectDeepCopy(obj) {
+    
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (obj instanceof Array) {
+        let copy = [];
+        for (let i = 0; i < obj.length; i++) {
+            copy[i] = makeObjectDeepCopy(obj[i]);
+        }
+        return copy;
+    }
+
+    if (obj instanceof Object) {
+        let copy = {};
+        for (let field in obj) {
+            if (obj.hasOwnProperty(field)) {
+                copy[field] = makeObjectDeepCopy(obj[field]);
+            }
+        }
+        return copy;
+    }
+}
+
+function selectFromInterval(arr, int1, int2){
+
+    if(!Array.isArray(arr) || arr.some((item) => (!Number.isInteger(item)))) {
+        throw new Error('Array is not valid or not an array')
+    }
+
+    if(!int1 || !int2 || !Number.isInteger(int1) || !Number.isInteger(int2)){
+        throw new Error('Interval is not valid')
+    }
+
+    let result = arr.filter((i)=> {
+        if(i >= (int1 > int2 ? int2 : int1) && i <= (int1 > int2 ? int1 : int2)){
+            return i;
+        }
+    });
+    return result;
+}
+
+const myIterable = {
+    from: 1,
+    to: 6,
+
+    [Symbol.iterator]() {
+
+        if (!Number.isInteger(this.from) 
+        || !Number.isInteger(this.to) 
+        || !this.hasOwnProperty('from') 
+        || !this.hasOwnProperty('to') 
+        || this.to<this.from) {
+            throw new Error('My Error')
+        }
+
+        this.current = this.from;
+        return this;
+      },
+    
+      next() {
+        if (this.current <= this.to) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+}
